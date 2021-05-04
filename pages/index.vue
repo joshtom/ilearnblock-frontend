f44490930374dff4c7ceef798cab9a622493ee7b
Joshua Olajide <joshuaolajide@Joshuas-MacBook-Pro.local>
2021-05-04 10:33:00 +0100

    Initial commit Fix antd library bundle size

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <a-button type="primary">Welcome to the hood</a-button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
