c78b45bc61c090a2ae5b96fce79105e45609653c
joshtom <olajidejoshua4real@gmail.com>
2021-05-14 23:25:09 +0100

    Bootstrap auth layout for login and register

---
+++ layouts/auth.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="auth-layout">
+    <div class="container">
+      <div class="row">
+        <div class="header">
+          <img
+            src="~/assets/images/logo.png"
+            alt="ilearnbock-logo"
+            width="70px"
+            height="70px"
+          />
+        </div>
+        <div class="body">
+          <h1>Learn amazing skills in Blockchain.</h1>
+          <p>
+            <nuxt-link to="/register" class="text-primary"> Sign Up </nuxt-link>
+            to acquire a skill in Blockchain
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <nuxt-child> </nuxt-child>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login-page">
+    <h1 class="title">Sign Up</h1>
+    <p class="subtitle">Sign in to create an account</p>
+    <Button>Sign in with Google</Button>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "auth",
+};
+</script>
